9d8c2fd1f1bd43d47d44304ef4a0f270e832b80b
abduxanon <abduxanonaxmadaliyev@gmail.com>
2024-03-09 23:46:23 +0500

    salom

---
+++ app.js
@@ -0,0 +1,82 @@
+//46-misol
+
+// function sliceArray(ary) {
+//   return ary.slice(0);
+// }
+// let myArray = [1, 2, 3];
+// console.log(sliceArray(myArray));
+
+//47-misol
+
+// function ovozlar(qolash, qolamaslig) {
+//   return qolash - qolamaslig;
+// }
+// let qolash = 13;
+// let qolamalig = 0;
+// console.log(ovozlar(qolash, qolamalig));
+
+//49-misol
+
+// function teskari(arr) {
+//     return arr.reverse();
+// }
+// var teskariArray1 = [1, 2, 3, 4];
+// var teskari2 = [9, 9, 2, 3, 4];
+// var teskari3 = [];
+
+// console.log(teskari(teskari1));
+// console.log(teskari(teskari2));
+// console.log(teskari(teskari3));
+
+//51-misol
+
+// function oshir1ga(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     arr[i] += 1;
+//   }
+//   return arr;
+// }
+// var oshir1 = [0, 1, 2, 3];
+// var oshir2 = [2, 4, 6, 8];
+// var oshir3 = [-1, -2, -3, -4];
+// console.log(oshir1ga(oshir1));
+// console.log(oshir1ga(oshir2));
+// console.log(oshir1ga(oshir3));
+
+//56-misol
+
+// function oxrgi(arr) {
+//   return arr[arr.length - 1];
+// }
+// let oxrgiArray1 = function oxrgiElement(arr) {
+//   return arr[arr.length - 1];
+// };
+// let Array1 = [1, 2, 3];
+// let Array2 = ["cat", "dog", "duck"];
+// let Array3 = ["true", "false", "true"];
+// console.log(oxrgi(Array1));
+// console.log(oxrgi(Array2));
+// console.log(oxrgi(Array3));
+
+//62-misol
+// function arystring(arr) {
+//   return arr.join("");
+// }
+
+// let arystring1 = ["a", "b", "c", "d", "e", "f"];
+// let arystring2 = [1, 2, 3, "a", "s", "dAAAA"];
+
+// console.log(arystring(arystring1));
+// console.log(arystring(arystring2));
+
+//63-misol
+
+// function qoshish(ary1, ary2) {
+//   return ary1.concat(ary2);
+// }
+// let massiv1 = [1, 3, 5];
+// let massiv2 = [2, 6, 8];
+// let natija = qoshish(massiv1, massiv2);
+// console.log(natija);
+
+//
